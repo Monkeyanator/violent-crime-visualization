22a8b94f2221ea29479de765db14888a172ff760
Sam Naser <sammnaser@gmail.com>
2017-03-21 23:30:02 -0400

    Removed crime data from html file.

---
+++ data.js
@@ -0,0 +1,86 @@
+var markers = [
+{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.164871, 'lng': -85.58541799999999}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.161423, 'lng': -85.7000438}}
+,{'crimeType': 'ANTI-FEMALE HOMOSEXUAL (LESBIAN)', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.2491532, 'lng': -85.5246475}}
+,{'crimeType': 'ANTI-JEWISH', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png', 'location': {'lat': 38.2836875, 'lng': -85.6320719}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.1071529, 'lng': -85.87474700000001}}
+,{'crimeType': 'ANTI-WHITE', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png', 'location': {'lat': 38.1282922, 'lng': -85.78617659999999}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.241611, 'lng': -85.78019210000001}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.2354191, 'lng': -85.521749}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.190016, 'lng': -85.62146899999999}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.182314, 'lng': -85.77519199999999}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.182314, 'lng': -85.77519199999999}}
+,{'crimeType': 'ANTI-JEWISH', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png', 'location': {'lat': 38.1959339, 'lng': -85.70780649999999}}
+,{'crimeType': 'ANTI-JEWISH', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png', 'location': {'lat': 38.1959339, 'lng': -85.70780649999999}}
+,{'crimeType': 'ANTI-FEMALE HOMOSEXUAL (LESBIAN)', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.233668, 'lng': -85.75859600000001}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.2369834, 'lng': -85.7183915}}
+,{'crimeType': 'ANTI-MALE HOMOSEXUAL (GAY)', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.263423, 'lng': -85.48754699999999}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.163023, 'lng': -85.577606}}
+,{'crimeType': 'ANTI-MALE HOMOSEXUAL (GAY)', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.1673093, 'lng': -85.6816332}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.105922, 'lng': -85.698453}}
+,{'crimeType': 'ANTI-WHITE', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png', 'location': {'lat': 38.14161860000001, 'lng': -85.69492559999999}}
+,{'crimeType': 'ANTI-ARAB', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png', 'location': {'lat': 38.2224945, 'lng': -85.692754}}
+,{'crimeType': 'ANTI-WHITE', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png', 'location': {'lat': 38.16858, 'lng': -85.68573599999999}}
+,{'crimeType': 'ANTI-ISLAMIC (MUSLIM)', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png', 'location': {'lat': 38.1385108, 'lng': -85.6608574}}
+,{'crimeType': 'ANTI-FEMALE HOMOSEXUAL (LESBIAN)', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.157849, 'lng': -85.580032}}
+,{'crimeType': 'ANTI-FEMALE HOMOSEXUAL (LESBIAN)', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.157849, 'lng': -85.580032}}
+,{'crimeType': 'ANTI-MALE HOMOSEXUAL (GAY)', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.2325553, 'lng': -85.7573874}}
+,{'crimeType': 'ANTI-FEMALE HOMOSEXUAL (LESBIAN)', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.29018, 'lng': -85.489734}}
+,{'crimeType': 'ANTI-FEMALE HOMOSEXUAL (LESBIAN)', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.29018, 'lng': -85.489734}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 37.7236675, 'lng': -87.1138175}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.2585643, 'lng': -85.700546}}
+,{'crimeType': 'ANTI-ARAB', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png', 'location': {'lat': 38.113284, 'lng': -85.863078}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.2400387, 'lng': -85.7239182}}
+,{'crimeType': 'ANTI-MALE HOMOSEXUAL (GAY)', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.2582303, 'lng': -85.6947623}}
+,{'crimeType': 'ANTI-JEWISH', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png', 'location': {'lat': 38.163897, 'lng': -85.57616}}
+,{'crimeType': 'ANTI-WHITE', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png', 'location': {'lat': 38.1946288, 'lng': -85.80413279999999}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.2836875, 'lng': -85.6320719}}
+,{'crimeType': 'ANTI-WHITE', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png', 'location': {'lat': 38.1676213, 'lng': -85.68421020000001}}
+,{'crimeType': 'ANTI-MALE HOMOSEXUAL (GAY)', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.2546838, 'lng': -85.7518278}}
+,{'crimeType': 'ANTI-MALE HOMOSEXUAL (GAY)', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.21128480000001, 'lng': -85.217148}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.2719548, 'lng': -85.8106643}}
+,{'crimeType': 'ANTI-MALE HOMOSEXUAL (GAY)', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.1744762, 'lng': -85.710278}}
+,{'crimeType': 'ANTI-FEMALE HOMOSEXUAL (LESBIAN)', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.157849, 'lng': -85.580032}}
+,{'crimeType': 'ANTI-WHITE', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png', 'location': {'lat': 38.2604783, 'lng': -85.8176505}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.1214129, 'lng': -85.873631}}
+,{'crimeType': 'ANTI-ARAB', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png', 'location': {'lat': 38.1719684, 'lng': -85.83527760000001}}
+,{'crimeType': 'ANTI-WHITE', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png', 'location': {'lat': 38.2546372, 'lng': -85.8091944}}
+,{'crimeType': 'ANTI-ARAB', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png', 'location': {'lat': 38.2528039, 'lng': -85.7574788}}
+,{'crimeType': 'ANTI-WHITE', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png', 'location': {'lat': 38.2529217, 'lng': -85.76797309999999}}
+,{'crimeType': 'ANTI-WHITE', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png', 'location': {'lat': 38.2690568, 'lng': -85.7889278}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.1937456, 'lng': -85.717044}}
+,{'crimeType': 'ANTI-TRANSGENDER', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.248914, 'lng': -85.777774}}
+,{'crimeType': 'ANTI-FEMALE HOMOSEXUAL (LESBIAN)', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.23874989999999, 'lng': -85.75798480000002}}
+,{'crimeType': 'ANTI-WHITE', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png', 'location': {'lat': 38.250294, 'lng': -85.7850316}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.287662, 'lng': -85.5209837}}
+,{'crimeType': 'ANTI-WHITE', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png', 'location': {'lat': 38.1078796, 'lng': -85.86822749999999}}
+,{'crimeType': 'ANTI-WHITE', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png', 'location': {'lat': 38.1852113, 'lng': -85.76916779999999}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.2578163, 'lng': -85.7043631}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.246621, 'lng': -85.731819}}
+,{'crimeType': 'ANTI-WHITE', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png', 'location': {'lat': 38.191525, 'lng': -85.792424}}
+,{'crimeType': 'ANTI-MALE HOMOSEXUAL (GAY)', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.2631137, 'lng': -85.5966145}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.1815289, 'lng': -85.65743289999999}}
+,{'crimeType': 'ANTI-GENDER NON-CONFORMING', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.1853998, 'lng': -85.60683300000001}}
+,{'crimeType': 'ANTI-WHITE', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png', 'location': {'lat': 38.1078796, 'lng': -85.86822749999999}}
+,{'crimeType': 'ANTI-ISLAMIC (MUSLIM)', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png', 'location': {'lat': 38.188384, 'lng': -85.617471}}
+,{'crimeType': 'ANTI-ISLAMIC (MUSLIM)', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png', 'location': {'lat': 38.278409, 'lng': -85.6003784}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.254918, 'lng': -85.757194}}
+,{'crimeType': 'ANTI-TRANSGENDER', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.254918, 'lng': -85.757194}}
+,{'crimeType': 'ANTI-JEWISH', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png', 'location': {'lat': 38.144101, 'lng': -85.863095}}
+,{'crimeType': 'ANTI-TRANSGENDER', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.237931, 'lng': -85.717856}}
+,{'crimeType': 'ANTI-ISLAMIC (MUSLIM)', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png', 'location': {'lat': 38.2509409, 'lng': -85.75583999999999}}
+,{'crimeType': 'ANTI-TRANSGENDER', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.1499369, 'lng': -85.5872593}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.1373262, 'lng': -85.6283974}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.195714, 'lng': -85.70947}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.099198, 'lng': -85.689375}}
+,{'crimeType': 'ANTI-ISLAMIC (MUSLIM)', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png', 'location': {'lat': 38.208784, 'lng': -85.6418194}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.1110274, 'lng': -85.64712019999999}}
+,{'crimeType': 'ANTI-GENDER NON-CONFORMING', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.1078796, 'lng': -85.86822749999999}}
+,{'crimeType': 'ANTI-WHITE', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png', 'location': {'lat': 38.1078796, 'lng': -85.86822749999999}}
+,{'crimeType': 'ANTI-MALE HOMOSEXUAL (GAY)', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.1715418, 'lng': -85.773382}}
+,{'crimeType': 'ANTI-JEWISH', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png', 'location': {'lat': 38.2240338, 'lng': -85.6104979}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.2064655, 'lng': -85.8617012}}
+,{'crimeType': 'ANTI-JEWISH', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png', 'location': {'lat': 38.2259169, 'lng': -85.6506062}}
+,{'crimeType': 'ANTI-BLACK', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', 'location': {'lat': 38.120971, 'lng': -85.632989}}
+,{'crimeType': 'ANTI-TRANSGENDER', 'markerUrl': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', 'location': {'lat': 38.144101, 'lng': -85.863095}}
+];
